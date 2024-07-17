75be21b3f97a7fda87dcfa50fd128614fe1017fa
bpratt <bpratt@ihmc.org>
2024-07-17 15:50:06 -0500

    Using new psyonicHands and remoteCaptury versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,14 +7,13 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
-    mavenLocal()
 }
 
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
-    implementation("us.ihmc:remotecaptury-java:1.0.998")
-    implementation("us.ihmc:psyonic-ability-hand-java:1.1.997")
+    implementation("us.ihmc:remotecaptury-java:1.0.4")
+    implementation("us.ihmc:psyonic-ability-hand-java:1.1.1")
     implementation("us.ihmc:euclid:0.21.0")
     implementation("us.ihmc:euclid-frame:0.21.0")
     implementation("us.ihmc:ihmc-yovariables:0.12.0")
